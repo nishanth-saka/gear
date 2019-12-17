e3808c8e2a5a1ecb280cef2c42c06974a4ecc526
nishanth-saka <nishanth-saka@users.noreply.github.com>
2019-12-17 17:41:28 +0530

    initial commit

---
+++ sketch.js
@@ -0,0 +1,71 @@
+let mobilenet;
+let video;
+let classifier;
+let sampleButton;
+let trainButton;
+let saveButton;
+let meButton;
+
+function modelReady() {
+  console.log('Model is ready!!!');  
+}
+
+function videoReady() {
+  console.log('Video is ready!!!');  
+}
+
+function whileTraining(loss){
+  if(!loss){
+    console.log('Training Complete!');
+    classifier.classify(gotResults);
+  }
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    classifier.classify(gotResults);
+  }
+}
+
+
+function setup() {
+  
+  video = createCapture(VIDEO);
+  video.hide();
+  
+  createCanvas(640, 480);
+  background(200);  
+  
+  mobilenet = ml5.featureExtractor('MobileNet', modelReady);
+  classifier = mobilenet.classification(video, videoReady);   
+  
+  console.log('Classifier');
+  console.log(classifier);
+  
+  sampleButton = createButton('pen');
+  sampleButton.mousePressed(function(args){
+    classifier.addImage('pen');
+  });
+  
+  meButton = createButton('me');
+  meButton.mousePressed(function(args){
+    classifier.addImage('me');
+  });
+  
+  trainButton = createButton('TRAIN');
+  trainButton.mousePressed(function(){
+    classifier.train(whileTraining);
+  });
+  
+  saveButton = createButton('Save');
+  saveButton.mousePressed(function(){
+    classifier.save();
+  });
+}
+
+function draw() {
+  image(video, 0, 0);
+}
