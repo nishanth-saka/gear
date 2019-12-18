17d48dcaaeca09f193bd9630255d3df9c92fba26
nishanth-saka <nishanth-saka@users.noreply.github.com>
2019-12-18 15:07:16 +0530

    face camera

--- sketch.js
+++ sketch.js
@@ -54,10 +54,9 @@ function setup() {
   var constraints = {
     audio: false,
     video: {
-      // facingMode: {
-      //   "exact": "environment"
-      // }
-      facingMode: "user"
+      facingMode: {
+        "exact": "environment"
+      }      
     }
   };
 
@@ -118,7 +117,8 @@ function setup() {
 }
 
 function draw() {
-  // image(video, 0, 0);
+  background(0);
+  image(video, 0, 0);
   fill(255);
   textSize(32);
   text(label, 10, height - 20);
