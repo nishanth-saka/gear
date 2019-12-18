5cc4860419e77da1d8fc2b7cbc090880497eff8c
nishanth-saka <nishanth-saka@users.noreply.github.com>
2019-12-18 17:11:03 +0530

    codetrain

--- sketch.js
+++ sketch.js
@@ -81,55 +81,55 @@ function setup() {
   // background(200);  
   
   //label = 'Initializing ml5..';
-  // mobilenet = ml5.featureExtractor('MobileNet', modelReady);
+  mobilenet = ml5.featureExtractor('MobileNet', modelReady);
   // //label = 'ml5 READY..';
 
   // //label = 'Initializing mobilenet..';
-  // classifier = mobilenet.classification(?video, videoReady);   
+  classifier = mobilenet.classification(?video, videoReady);   
   // //label = 'mobilenet READY..';
   
-  // sampleButton = createButton('Turbine');
-  // sampleButton.mousePressed(function(args){
-  //   classifier.addImage('Turbine');    
+  sampleButton = createButton('Turbine');
+  sampleButton.mousePressed(function(args){
+    classifier.addImage('Turbine');    
 
-  //   //label = 'Turbine';     
-  // });
+    //label = 'Turbine';     
+  });
   
-  // meButton = createButton('Helicopter');
-  // meButton.mousePressed(function(args){
-  //   let res = classifier.addImage(video, 'Turbine', addedImage);        
-  //   //label = 'Helicopter';     
-  // });
+  meButton = createButton('Helicopter');
+  meButton.mousePressed(function(args){
+    let res = classifier.addImage(video, 'Turbine', addedImage);        
+    //label = 'Helicopter';     
+  });
   
-  // trainButton = createButton('TRAIN');
-  // trainButton.mousePressed(function(){
-  //   console.log('Training Begins...');
-  //   classifier.train(whileTraining)
-  //   .then(function () { 
-  //       console.log('Success, You are a GEEK'); 
-  //   })
-  //   .catch(function (err) { 
-  //       console.log('Some error has occured'); 
-  //       console.log(err);
-  //   }); 
+  trainButton = createButton('TRAIN');
+  trainButton.mousePressed(function(){
+    console.log('Training Begins...');
+    classifier.train(whileTraining)
+    .then(function () { 
+        console.log('Success, You are a GEEK'); 
+    })
+    .catch(function (err) { 
+        console.log('Some error has occured'); 
+        console.log(err);
+    }); 
     
         
-  // });
+  });
   
-  // saveButton = createButton('Check');
-  // saveButton.mousePressed(function(){
-  //   // classifier.save();
-  //   classifier.classify(video)
-  //   .then(function (obj) { 
-  //       console.log('Success, You are a GEEK'); 
-  //       console.log(obj);
-  //   })
-  //   .catch(function (err) { 
-  //       console.log('Some error has occured'); 
-  //       console.log(err);
-  //   }); ;
-  // //   label = 'Reset';    
-  // });
+  saveButton = createButton('Check');
+  saveButton.mousePressed(function(){
+    // classifier.save();
+    classifier.classify(video)
+    .then(function (obj) { 
+        console.log('Success, You are a GEEK'); 
+        console.log(obj);
+    })
+    .catch(function (err) { 
+        console.log('Some error has occured'); 
+        console.log(err);
+    }); ;
+  //   label = 'Reset';    
+  });
 }
 
 function draw() {
