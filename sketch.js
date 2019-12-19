1fe2e17e59a1a0157ea142ebd2f81d4fe36061e2
vipul parmar <vipul.parmar@xcubelabs.com>
2019-12-19 18:20:29 +0530

    changed the ui and implemented the  network check.

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ let trainButton;
 let saveButton;
 let meButton;
 let label = '';
+let infoDiv;
+let isClicked = false;
 
 var w = 640;
 var h = 480;
@@ -25,7 +27,7 @@ var h = 480;
 // function videoReady() {
 //   console.log('Video is ready!!!');  
 //   console.log('Adding Image...');  
-  
+
 //   // classifier.addImage(video,'Turbine')
 //   //   .then(function (obj) { 
 //   //       console.log('Image ready...');               
@@ -36,15 +38,15 @@ var h = 480;
 //   //   });  
 // }
 
-function whileTraining(loss){
+function whileTraining(loss) {
   console.log('Training...');
   console.log(loss);
-  
-  if(!loss){
+  label = 'Training Please wait...';
+  if (!loss) {
     //label = 'Training Complete!';
-    label = 'DONE!';     
+    label = 'DONE!';
     console.log('Training Complete!');
-    label = '';     
+    label = '';
   }
 }
 
@@ -67,94 +69,128 @@ function addedImage(res, err) {
 
 function setup() {
 
-  createCanvas(480, 480);
-  
+  createCanvas(768, 480);
+
   let constraints = {
     video: {
       mandatory: {
         minWidth: 1280,
         minHeight: 720
       },
-      optional: [{ maxFrameRate: 10 }]
+      optional: [{ maxFrameRate: 10 }],
+      facingMode: 'environment'
     },
     audio: false
   };
+  // infoDiv = createDiv(label);
+  // infoDiv.position(30,450);
 
-  mobilenet = ml5.featureExtractor('MobileNet', function(){
+  mobilenet = ml5.featureExtractor('MobileNet', function () {
 
   });
 
 
   console.log('capturing video..');
 
-  video = createCapture(constraints, function(stream) {
-  
+  video = createCapture(constraints, function (stream) {
+
     console.log('creating classifier..');
 
-    classifier = mobilenet.classification(video, function(){
-        video.hide();
-        console.log('all set!');          
-    });       
+    classifier = mobilenet.classification(video, function () {
+      video.hide();
+      console.log('all set!');
+    });
   });
 
   video.elt.setAttribute('playsinline', '');
   video.hide();
-  
 
+
+
+  sampleButton = createButton('Object A');
+  let col = color(25, 23, 200, 50);
+  sampleButton.style('background-color', col);
+  sampleButton.position(10, 500);
   
-  sampleButton = createButton('Turbine');
-  sampleButton.mousePressed(function(args){
-    label = 'Training.. Turbine';     
-     classifier.addImage(video, 'Turbine', function(){
-      console.log('Turbine...'); 
-      label = '';     
-     });   
-         
+  sampleButton.mousePressed(function (args) {
+    label = 'Capturing Object A Please wait...';
+  isClicked =true;
+    setTimeout(() => {
+    classifier.addImage(video, 'Object A', function () {
+      console.log('Object A...');
+      label = '';
+    });
+   }, 0);
   });
-  
-  meButton = createButton('Helicopter');
-  meButton.mousePressed(function(args){
-    label = 'Training.. Helicopter';  
-    classifier.addImage(video, 'Helicopter', function(){
-      console.log('Helicopter...'); 
-      label = '';     
-     });   
+
+  meButton = createButton('Object B');
+  meButton.style('background-color', col);
+  meButton.position(100, 500);
+  meButton.mousePressed(function (args) {
+    label = 'Capturing Object B Please wait...';
+    isClicked =true;
+    setTimeout(() => {
+    classifier.addImage(video, 'Object B', function () {
+      console.log('Object B...');
+      label = '';
+    });
+   }, 0);
   });
-  
-  trainButton = createButton('TRAIN');
-  trainButton.mousePressed(function(){
-    console.log('Training Begins...');
-    label = 'Training Begins...';  
-    classifier.train(whileTraining);
+
+  trainButton = createButton('Train');
+  trainButton.style('background-color', col);
+  trainButton.position(200, 500);
+
+  trainButton.mousePressed(function () {
+    console.log('Training Begins Please wait...');
+    
+  if(isClicked){
+    try {
+      label = 'Training Begins Please wait...'; 
+      classifier.train(whileTraining);  
+    } catch (err) {
+      console.log(err);
+      label = 'generic error';
+    }
+
+  } else {
+    label = 'Please capture the images for training.'
+  }
   });
-  
-  saveButton = createButton('Check');
-  saveButton.mousePressed(function(){
+
+  saveButton = createButton('Test');
+  saveButton.style('background-color', col);
+  saveButton.position(280, 500);
+
+  saveButton.mousePressed(function () {
     label = '';    // classifier.save( );
     classifier.classify(video)
-    .then(function (obj) { 
-        console.log('Success, You are a GEEK'); 
+      .then(function (obj) {
+        console.log('Success, You are a GEEK');
         console.log(obj[0].label);
         label = obj[0].label;
-    })
-    .catch(function (err) { 
-        console.log('Some error has occured'); 
+      })
+      .catch(function (err) {
+        console.log('Some error has occured');
         console.log(err);
-        label = err;   
-    }); ;
-  //   label = 'Reset';    
+        label = err;
+      });;
+    //   label = 'Reset';    
   });
 }
 
 function draw() {
   image(video, 0, 0, 1024, 480);
 
-  // background(0);
+  //  background(255,255,255);  
   // image(video, 0, 0, 320, 240);
   // fill(255);
-  textSize(16);
-  text(label, 10, height - 10);
-
+  textSize(22);
+  fill(255,255,255);
+  text(label, 30, 450);
+  if(!window.navigator.onLine){
+    label = 'Please check internet connection.';
+  }
   // image(video, 0, 0, width, width * video.height / video.width)
   // .then(function (obj) { 
   //     console.log('Success, You are a GEEK');       
